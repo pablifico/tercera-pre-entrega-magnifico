645375ef4d2ac318904250102976ce3882641028
Pablo Magnifico <nuevosairespablo@hotmail.com>
2023-04-11 21:32:54 -0300

    Update urls.py

--- inicio/urls.py
+++ inicio/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.mi_vista, name='inicio'),
 
     path('crear-jugador/', views.crear_jugador, name='crear_jugador'),
+    path('jugadores/', views.lista_jugadores, name='listar_jugadores'),
 
 ]
